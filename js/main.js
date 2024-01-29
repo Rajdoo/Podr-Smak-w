726038223f73c0bf7ddaaae770c301469d8de16b
Zuzanna <zuzanna.spychalska05@gmail.com>
2024-01-29 21:20:53 +0100

    added contact section. Modify header.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 const navMobile = document.querySelector('.nav-mobile__list');
 const navBtn = document.querySelector('.burger-btn');
+const nav = document.querySelector('.nav');
 
 
 const handleNav = () => {
     navMobile.classList.toggle('nav-mobile__list--active');
 }
 
+const shadowNav = () => {
+    if(window.scrollY >= 300){
+        console.log('yooo')
+    }else{
+        
+    }
+}
+
+
 navBtn.addEventListener('click', handleNav);
+window.addEventListener('scroll', shadowNav);
